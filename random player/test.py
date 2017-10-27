bce381c0c5df7a3782369791351dd7027174bec1
Vishal Subbiah <vishalsubbiah@DN0a221759.SUNet>
2017-10-26 17:24:57 -0700

    figured out actions

--- random player/test.py
+++ random player/test.py
@@ -1,24 +1,29 @@
 import gym
-
+import matplotlib.pyplot as plt
+#31 steps per no (for 0)
+#0 - none (31 steps)
+#1 - accelerate (18 steps)
+#2- go right (30 steps)
+#3-go left (32 steps)
+#4- slow? (33 steps)
+#5- slow and right (34 steps)
+#6- slow and left (34 steps)
+#7- accelerate and right (16 steps)
+#8- accelrate and left (17 steps)
 game_choice='MountainCar-v0'
 env = gym.make('Enduro-v0')
-
-for i_episode in range(2):
+name="eight/"
+for i_episode in range(1):
 	observation = env.reset()
-	for t in range(1000):
+	for t in range(100):
 		env.render()
-		state=observation[1]
-		#print("state: ",state)
+		state=observation
+		plt.imshow(observation)
+		plt.savefig(name+"img"+str(t))
 		action=env.action_space.sample()
-		#print(env.action_space)
-		#action=1
-		#if(state>0):
-		#	action=2
-		#else:
-		#	action=0
-		print("action:",action)
+		action=8
 		observation,reward,done,info=env.step(action)
-		reward=observation[0]
-		#print("reward: ",reward)
+		if(reward !=0):
+			print("reward: ",reward)
 		if done:
 			print("episode finished after {} timesteps".format(t+1))
